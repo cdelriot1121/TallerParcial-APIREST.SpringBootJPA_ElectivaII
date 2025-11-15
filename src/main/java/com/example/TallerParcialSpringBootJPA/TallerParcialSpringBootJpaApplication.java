67a7dea2da3403abf275cf87fb031537f74213c3
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-11-15 13:39:05 -0500

    First commit

---
+++ src/main/java/com/example/TallerParcialSpringBootJPA/TallerParcialSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TallerParcialSpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerParcialSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerParcialSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TallerParcialSpringBootJPA/TallerParcialSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TallerParcialSpringBootJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerParcialSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
